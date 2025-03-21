c8c0e02f93aaf7408ae807bfa39e77d19d3d48a2
vladislavalerievich <vlad.moroshan@gmail.com>
2025-03-21 14:35:49 +0100

    Update model name

--- src_torch/training/utils.py
+++ src_torch/training/utils.py
@@ -23,7 +23,7 @@ def position_encoding(periods: int, freqs: int):
     ])
 
 def generate_model_save_name(config):
-    name = 'latest_'
+    name = 'GatedDeltaNet_Helix_A100_5_Heads_'
     if config['model_type'] == 'ssm' and config['ssm_config']['bidirectional']:
         name += 'bi_'
     name += f'{config["version"][:2]}_'
