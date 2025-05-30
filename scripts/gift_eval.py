7ee1883d7a2d4bc8907f4401d7f3fecae79c14f0
vladislavalerievich <vlad.moroshan@gmail.com>
2025-05-30 07:45:27 +0200

    update toml

---
+++ scripts/gift_eval.py
@@ -0,0 +1,114 @@
+import os
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+from dotenv import load_dotenv
+from gift_eval.data import Dataset
+from gluonts.dataset.util import to_pandas
+
+# Load environment variables
+load_dotenv()
+
+# Get the GIFT_EVAL path from environment variables
+gift_eval_path = os.getenv("GIFT_EVAL")
+
+if gift_eval_path:
+    # Convert to Path object for easier manipulation
+    gift_eval_path = Path(gift_eval_path)
+
+    # Get all subdirectories (dataset names) in the GIFT_EVAL path
+    dataset_names = []
+    for dataset_dir in gift_eval_path.iterdir():
+        if dataset_dir.name.startswith("."):
+            continue
+        if dataset_dir.is_dir():
+            freq_dirs = [d for d in dataset_dir.iterdir() if d.is_dir()]
+            if freq_dirs:
+                for freq_dir in freq_dirs:
+                    dataset_names.append(f"{dataset_dir.name}/{freq_dir.name}")
+            else:
+                dataset_names.append(dataset_dir.name)
+
+    print("Available datasets in GIFT_EVAL:")
+    for name in sorted(dataset_names):
+        print(f"- {name}")
+else:
+    print(
+        "GIFT_EVAL path not found in environment variables. Please check your .env file."
+    )
+
+
+ds_name = "m4_monthly"  # Name of the dataset
+to_univariate = False  # Whether to convert the data to univariate
+term = "short"  # Term of the dataset
+
+dataset = Dataset(name=ds_name, term=term, to_univariate=to_univariate)
+print("Dataset frequency: ", dataset.freq)
+print("Prediction length: ", dataset.prediction_length)
+print("Number of windows in the rolling evaluation: ", dataset.windows)
+
+
+train_data_iter = dataset.training_dataset  # Get the training data iterator
+
+train_data = next(iter(train_data_iter))
+print("Keys in the training data: ", train_data.keys())
+
+print("Item id: ", train_data["item_id"])
+print("Start Date: ", train_data["start"])
+print("Frequency: ", train_data["freq"])
+print("Last 10 target values: ", train_data["target"][-10:])
+
+
+train_series = to_pandas(train_data)
+train_series.plot()
+plt.grid(which="both")
+plt.legend(["train series"], loc="upper left")
+plt.show()
+
+
+val_data_iter = dataset.validation_dataset
+
+val_data = next(iter(val_data_iter))
+print("Keys in the validation data: ", val_data.keys())
+
+print("Item id: ", val_data["item_id"])
+print("Start Date: ", val_data["start"])
+print("Frequency: ", val_data["freq"])
+print("Last 10 target values: ", val_data["target"][-10:])
+
+val_series = to_pandas(val_data)
+val_series.plot()
+plt.grid(which="both")
+# Add a vertical axis for where the train series ends
+plt.axvline(
+    x=train_series.index[-1], color="r", linestyle="--", label="End of train series"
+)
+plt.legend(["val series", "end of train series"], loc="upper left")
+plt.show()
+
+
+test_split_iter = dataset.test_data
+test_data = next(iter(test_split_iter))
+
+test_input_split_iter = dataset.test_data.input
+
+input = next(iter(test_input_split_iter))
+print("Keys in the test data: ", input.keys())
+
+print("\n\nContext Item id: ", input["item_id"])
+print("Context Start Date: ", input["start"])
+print("Context Frequency: ", input["freq"])
+print("Context Last 10 target values: ", input["target"][-10:])
+
+test_label_split_iter = dataset.test_data.label
+label = input = next(iter(test_label_split_iter))
+print("\n\nForecast Item id: ", label["item_id"])
+print("Forecast Start Date: ", label["start"])
+print("Forecast Frequency: ", label["freq"])
+print("Forecast Last 10 target values: ", label["target"][-10:])
+
+test_series = to_pandas(test_data[1])
+test_series.plot()
+plt.grid(which="both")
+plt.legend("test series", loc="upper left")
+plt.show()
