a3733c74e449e10adcdc1eeec51fb228c77c349d
vladislavalerievich <vlad.moroshan@gmail.com>
2025-06-27 13:38:32 +0200

    Add __init__ file to sine wave generation  module

---
+++ src/synthetic_generation/sine_waves/__init__.py
@@ -0,0 +1,6 @@
+"""Sine wave synthetic time series generation module."""
+
+from .sine_wave_generator import SineWaveGenerator
+from .sine_wave_generator_wrapper import SineWaveGeneratorWrapper
+
+__all__ = ["SineWaveGenerator", "SineWaveGeneratorWrapper"]
